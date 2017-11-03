db0142988d46d23a2010bf3277333c8593375d65
Tibaldi De Jesus Gonzalez <phittipaldi@gmail.com>
2017-11-03 06:33:14 -0400

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -13,9 +15,34 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            "Enter a to-do item"
+        )
+
+        inputbox.send_keys('Buy peacock feathers')
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+
+        self.assertTrue(
+            any(row.text == 'Buy peacock feathers' for row in rows)
+        )
+
+        self.fail('Finish the test!')
 
 # if __name__ == '__main__':
 #     unittest.main(warnings='ignore')
+
+# header_text = self.browser.find_element_by_tag_name('h1').text
