30ffae6819f2f8d99daa79bd46490d43c0377271
Tibaldi De Jesus Gonzalez <phittipaldi@gmail.com>
2017-11-08 15:51:37 -0400

    change testcase to LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,20 +1,27 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
+# import unittest
+
+MAX_WAIT = 10
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
 
-    def tearDown(self):
-        self.browser.quit()
+    # def tearDown(self):
+    #     self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get(self.live_server_url)
 
-        self.browser.get('http://localhost:8000')
+        # self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -32,22 +39,45 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy peacock feathers')
 
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        # time.sleep(1)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = self.browser.find_elements_by_tag_name("tr")
-        # rows = table.find_element_by_tag_name('tr')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear. Cont. were:{}".format(table.text)
-        )
+        # The page updates again, and now shows both items on her list
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows])
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = self.browser.find_elements_by_tag_name("tr")
+
+        # self.assertTrue(
+        #     any(row.text == '1: Buy peacock feathers' for row in rows),
+        #     "New to-do item did not appear. Cont. were:{}".format(table.text)
+        # )
+
+        # self.assertIn(
+        #     '2: Use peacock feathers to make a fly',
+        #     [row.text for row in rows])
 
         self.fail('Finish the test!')
+
+    def wait_for_row_in_list_table(self, row_text):
+
+        start_time = time.time()
+        while True:
+            try:
+                # table = self.browser.find_element_by_id('id_list_table')
+                rows = self.browser.find_elements_by_tag_name("tr")
+                # rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
 # if __name__ == '__main__':
 #     unittest.main(warnings='ignore')
